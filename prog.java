30df3d0e105bea7c4c159aeec2cfd3edcdb36272
kavish <kavish@itc>
2018-06-26 06:33:03 +0000

    added a+b

--- prog.java
+++ prog.java
@@ -1,9 +1,11 @@
-import java.io.*;
+ import java.io.*;
 
  class prog
   {
      public static void main(String args[]);
-	{
+	{ 
+               int a,b;
+               int c = a+b;
 	   System.out.println("Hello World");
 	}
 
