f9e1833c46d1be2338de670afb707d56b5af74bd
Team4 <Team4@ITC>
2018-06-26 08:52:22 +0000

    divided new

--- prog.java
+++ prog.java
@@ -8,8 +8,12 @@
                int c = a+b;
 	   System.out.println("Hello World");
            System.out.println(c);
-		int d=a*b;	
+		int d=a*b;
+		System.out.println(d);	
+		int e= a/b;
+		System.out.println(e);
 	}
+
 
    }
 
