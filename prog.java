3618800b3d007e54025f9a66fd0ae20a023d84c7
Kavish <Kavish@itc>
2018-06-26 06:51:38 +0000

    multiplied a & b

--- prog.java
+++ prog.java
@@ -4,10 +4,11 @@
   {
      public static void main(String args[]);
 	{ 
-               int a,b;
+               int a=7,b=9;
                int c = a+b;
 	   System.out.println("Hello World");
-                 System.out.println(c);
+           System.out.println(c);
+		int d=a*b;	
 	}
 
    }
