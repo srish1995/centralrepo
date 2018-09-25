21df90facc4c5c7465af7fa5e859b9475a358a4f
Kavish Narula <28574@ITCINFOTECH.COM>
2018-09-25 12:29:21 +0530

    multiplication deleted

--- prog.java
+++ prog.java
@@ -7,15 +7,14 @@
                int a=18,b=7;
                int c = a+b;
 	   System.out.println("Basic Maths");
-           System.out.println("Addition"+c);
-		int d=a*b;
-		System.out.println("Multiplication :"+d);	
+           System.out.println("Addition"+c);	
 		int e= a/b;
 		System.out.println("Division"+e);
 		int sub=a-b;
 		System.out.println("Subtraction"+sub);
 		int mod= a%b;
 		System.out.println("Mod"+mod);
+
 	}
 
 
