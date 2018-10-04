5e440ad25f68ab87040a733122fccf895bf561a8
Kavish Narula <28574@ITCINFOTECH.COM>
2018-10-04 11:29:37 +0530

    values changed

--- prog.java
+++ prog.java
@@ -4,7 +4,7 @@
   {
      public static void main(String args[])
 	{ 
-               int a=18,b=7;
+               int a=50,b=10;
                int c = a+b;
 	   System.out.println("Basic Maths");
            System.out.println("Addition"+c);	
@@ -16,8 +16,6 @@
 		System.out.println("Mod"+mod);
 		int mul = a*b;
 		System.out.println("multiplication"+mul);
-		int app = mul-sub;
-		System.out.println("subtraction of mul sub"+app);
 
 	}
 
